21798d8d09c7fb8ccc8753a99e48a1e42e700ba3
zhangsr16 <145007394+zhangsr16@users.noreply.github.com>
2025-03-12 14:28:04 +0800

    Update setDataset.py

--- MixModels/setDataset.py
+++ MixModels/setDataset.py
@@ -1,7 +1,16 @@
-
 import pandas as pd
 import numpy as np
 import os
+
+
+def give_rows(df_list, key_column, common_rows):
+    # 遍历所有DataFrame，删除差异行
+    updated_dfs = []
+    for df in df_list:
+        df = df[df[key_column].isin(common_rows)]
+        updated_df = df.reset_index(drop=True)
+        updated_dfs.append(updated_df)
+    return updated_dfs
 
 
 def scale_rows(df_list, key_column):
@@ -35,20 +44,20 @@ def spilt_dataset(union_dfs, dataset_folder, column_for_diff, exclude_columns):
         if column in exclude_columns:
             diff_df[column] = df1[column]  # 直接复制不参与差值计算的列
         else:
-            diff_df[column] = df1[column] - df2[column]
+            diff_df[column] = (df1[column] - df2[column]) / df2[column]
 
     # 根据指定列的差值将数据分为两份
-    positive_diff = diff_df[column_for_diff] > 0
-    negative_diff = diff_df[column_for_diff] <= 0
+    positive_diff = diff_df[column_for_diff] > 0.04
+    negative_diff = diff_df[column_for_diff] < 0.02
     pos = 0
     for df in union_dfs:
         # 列交换
-        df.insert(0, '所属行业_temp', df['所属行业'])
-        df.insert(0, 'DATE_temp', df['DATE'])
-        df = df.drop('所属行业', axis=1)
-        df = df.drop('DATE', axis=1)
-        df = df.rename(columns={'所属行业_temp': '所属行业'})
-        df = df.rename(columns={'DATE_temp': 'DATE'})
+        # df.insert(0, '所属行业_temp', df['所属行业'])
+        # df.insert(0, 'DATE_temp', df['DATE'])
+        # df = df.drop('所属行业', axis=1)
+        # df = df.drop('DATE', axis=1)
+        # df = df.rename(columns={'所属行业_temp': '所属行业'})
+        # df = df.rename(columns={'DATE_temp': 'DATE'})
 
         Up_dataset = df[positive_diff]
         Other_dataset = df[negative_diff]
@@ -57,6 +66,7 @@ def spilt_dataset(union_dfs, dataset_folder, column_for_diff, exclude_columns):
         pos += 1
     return len(Up_dataset)
 
+
 # # Area
 # column_for_diff = '占市场'  # 根据你需要的列名进行修改
 # exclude_columns = ['序号', '地区']  # 需要排除的列名列表，根据实际情况修改
@@ -64,17 +74,24 @@ def spilt_dataset(union_dfs, dataset_folder, column_for_diff, exclude_columns):
 column_for_diff = '最高'  # 根据你需要的列名进行修改
 exclude_columns = ['代码', '名称', '所属行业', 'DATE']  # 需要排除的列名列表，根据实际情况修改
 
-data_folder = 'F:/Desktop/THS/data/test2/'
-dataset_folder = './Training_set/'
-seqlen = 8
+data_folder = './Training_set/APP_0/'
+dataset_folder = './Testing_set/'
+seqlen = 4
 
 label_files = [file_name for file_name in os.listdir(data_folder)]
 df_list = []
 # label_files[-seqlen:]，文件内容仅为xlsx时
-for label_file in label_files:
+for label_file in label_files[-seqlen:]:
     if label_file.endswith('.xlsx'):
         df = pd.read_excel(data_folder + label_file)
         df_list.append(df)
+
+# given rows
+with open('zsr.txt', 'r', encoding='utf-8') as f:
+    lines = f.readlines()
+lines = [lines.strip() for lines in lines]
+union_dfs = give_rows(df_list, '代码', lines)
+# auto rows
 union_dfs = scale_rows(df_list, '代码')
 
 # 生成数据集
