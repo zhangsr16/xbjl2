59f26b370b20f4d7e4abe5972784bcc73bb90b24
zhangsr16 <145007394+zhangsr16@users.noreply.github.com>
2025-04-01 16:02:11 +0800

    Update model.py

--- MixModels/model.py
+++ MixModels/model.py
@@ -37,6 +37,8 @@ class Model(nn.Module):
         self.feature_dim = input_dim['feature_dim']  # SeqLen
         self.dropout_feature = nn.Dropout(config["trainer_dropout_feature"])
         self.dropout_hidden = nn.Dropout(config["trainer_dropout_hidden"])
+        self.classifier_precision = config["classifier_precision"]
+        self.classifier_recall = config["classifier_recall"]
         self.dtype = torch.float
 
         # Transformer
@@ -96,71 +98,118 @@ class Model(nn.Module):
         """
         Start_time = time.time()
         # batch, feature, seq, cluster_dim, cyc
-        x, env = x_env
-        x = x[..., 1:]
-        x = torch.nan_to_num(x, nan=0.0, posinf=0.0, neginf=0.0)
-        x = torch.concat((x, env), axis=1).to(self.dtype)
-        # Masks
-        src_mask = nn.Transformer.generate_square_subsequent_mask(self.feature_dim, dtype=torch.bool)
-        src_key_padding_mask = torch.zeros(x.shape[0], self.feature_dim, dtype=torch.bool)
-        memory_key_padding_mask = torch.zeros(x.shape[0], self.feature_dim, dtype=torch.bool)
-        tgt_mask = nn.Transformer.generate_square_subsequent_mask(self.ResSeqLen).type(torch.bool)
-        tgt_key_padding_mask = torch.zeros(x.shape[0], self.ResSeqLen, dtype=torch.bool)
+        x, env, classifiers, classifiers_RPscore = x_env
+        batch_size = x.shape[0]
+        RPscore = [[i[0][0] for i in classifiers_RPscore], [j[1][0] for j in classifiers_RPscore]]
+        precision_col = []
+        recall_col = []
+        for i in range(len(RPscore[0])):
+            if RPscore[0][i] > self.classifier_precision:
+                precision_col.append(i)
+                pass
+            if RPscore[1][i] > self.classifier_recall:
+                recall_col.append(i)
+                pass
+        # Precision Forward
+        precision_row = []
+        recall_row = []
+        raw_row = []
+        for i in range(x.shape[0]):
+            pred = max(classifiers[i][precision_col])
+            recall = max(classifiers[i][recall_col])
+            if pred > 0:
+                precision_row.append(i)
+            elif recall > 0:
+                recall_row.append(i)
+            else:
+                raw_row.append(i)
+        x_pred_logit = torch.zeros(batch_size, self.output_dim)
+        pos = 0
+        for i in range(batch_size):
+            if i in precision_row:
+                x_pred_logit[i][1] = 1.0
+            else:
+                x_pred_logit[i][0] = 1.0
+            pos += 1
+        # Recall DeepModels Forward
+        if len(recall_row) < 4 or len(raw_row) < 4:
+            recall_row = recall_row + raw_row
+            raw_row = []
+        for r in [recall_row, raw_row]:
+            if len(r) > 1:
+                x = x[r][..., 1:]
+                x = torch.nan_to_num(x, nan=0.0, posinf=0.0, neginf=0.0)
+                x = torch.concat((x, env[r]), axis=1).to(self.dtype)
 
-        # Encode_TF
-        start_time = time.time()
-        TFencoded = (
-            self.transformer_encoder(x.permute(2, 0, 1), mask=src_mask,
-                                     src_key_padding_mask=src_key_padding_mask)).permute(1, 2, 0)
-        TFencoded = (self.embed(TFencoded.permute(0, 2, 1))).permute(0, 2, 1)
-        end_time = time.time()
-        TFencoded_time = end_time - start_time
+                # Masks
+                src_mask = nn.Transformer.generate_square_subsequent_mask(self.feature_dim, dtype=torch.bool)
+                src_key_padding_mask = torch.zeros(x.shape[0], self.feature_dim, dtype=torch.bool)
+                memory_key_padding_mask = torch.zeros(x.shape[0], self.feature_dim, dtype=torch.bool)
+                tgt_mask = nn.Transformer.generate_square_subsequent_mask(self.ResSeqLen).type(torch.bool)
+                tgt_key_padding_mask = torch.zeros(x.shape[0], self.ResSeqLen, dtype=torch.bool)
 
-        # Encode_CNN
-        start_time = time.time()
-        CnnRes = self.Cnn(x)
-        end_time = time.time()
-        CnnRes_time = end_time - start_time
+                # Encode_TF
+                start_time = time.time()
+                TFencoded = (
+                    self.transformer_encoder(x.permute(2, 0, 1), mask=src_mask,
+                                             src_key_padding_mask=src_key_padding_mask)).permute(1, 2, 0)
+                TFencoded = (self.embed(TFencoded.permute(0, 2, 1))).permute(0, 2, 1)
+                end_time = time.time()
+                TFencoded_time = end_time - start_time
 
-        # Encode_RNN
-        start_time = time.time()
-        RnnRes = (self.Rnn(x.permute(0, 2, 1))).permute(0, 2, 1)
-        end_time = time.time()
-        RnnRes_time = end_time - start_time
+                # Encode_CNN
+                start_time = time.time()
+                CnnRes = self.Cnn(x)
+                end_time = time.time()
+                CnnRes_time = end_time - start_time
 
-        # Decode_Total
-        start_time = time.time()
-        TFsrc = torch.concat((CnnRes, RnnRes), axis=2)
-        TFdecoded = (self.transformer_decoder(TFsrc.permute(2, 0, 1), TFencoded.permute(2, 0, 1),
-                                              tgt_mask=tgt_mask,
-                                              tgt_key_padding_mask=tgt_key_padding_mask,
-                                              memory_key_padding_mask=memory_key_padding_mask)).permute(1, 2, 0)
-        end_time = time.time()
-        TFdecoded_time = end_time - start_time
+                # Encode_RNN
+                start_time = time.time()
+                RnnRes = (self.Rnn(x.permute(0, 2, 1))).permute(0, 2, 1)
+                end_time = time.time()
+                RnnRes_time = end_time - start_time
 
-        # TF
-        start_time = time.time()
-        TFsrc = torch.concat((TFsrc, TFdecoded), axis=2)
-        tgtshape = torch.zeros(x.shape[0], self.hidden_dim * self.feature_dim,
-                               self.hidden_dim * self.feature_dim * self.ResSeqLen, dtype=self.dtype)
-        TFRes = (self.TF(TFsrc.permute(2, 0, 1), tgtshape.permute(2, 0, 1))).permute(1, 2, 0)
-        TFRes = TFRes.reshape(TFRes.shape[0], -1)
-        end_time = time.time()
-        TFRes_time = end_time - start_time
+                # Decode_Total
+                start_time = time.time()
+                TFsrc = torch.concat((CnnRes, RnnRes), axis=2)
+                TFdecoded = (self.transformer_decoder(TFsrc.permute(2, 0, 1), TFencoded.permute(2, 0, 1),
+                                                      tgt_mask=tgt_mask,
+                                                      tgt_key_padding_mask=tgt_key_padding_mask,
+                                                      memory_key_padding_mask=memory_key_padding_mask)).permute(1, 2, 0)
+                end_time = time.time()
+                TFdecoded_time = end_time - start_time
 
-        # FC output
-        hidden = self.dropout_hidden(TFRes)
-        hidden = self.fc(hidden)
-        hidden = self.bn(hidden)
-        hidden = self.relu(hidden)
-        hidden = self.dropout_feature(hidden)
-        logits = self.output(hidden)
-        logits = logits.reshape(logits.shape[0], -1)
+                # branch
 
-        End_time = time.time()
-        Total_time = End_time - Start_time
-        # print(f"Time Records:\nTotal :{Total_time}, TFencoded:{TFencoded_time}, CnnRes:{CnnRes_time}, RnnRes:{RnnRes_time}, TFdecoded:{TFdecoded_time}, TFRes:{TFRes_time}")
-        return F.softmax(logits, dim=1)
+                # Transform
+                start_time = time.time()
+                TFsrc = torch.concat((TFsrc, TFdecoded), axis=2)
+                tgtshape = torch.zeros(x.shape[0], self.hidden_dim * self.feature_dim,
+                                       self.hidden_dim * self.feature_dim * self.ResSeqLen, dtype=self.dtype)
+                TFRes = (self.TF(TFsrc.permute(2, 0, 1), tgtshape.permute(2, 0, 1))).permute(1, 2, 0)
+                TFRes = TFRes.reshape(TFRes.shape[0], -1)
+                end_time = time.time()
+                TFRes_time = end_time - start_time
+
+                # FC output
+                hidden = self.dropout_hidden(TFRes)
+                hidden = self.fc(hidden)
+                hidden = self.bn(hidden)
+                hidden = self.relu(hidden)
+                hidden = self.dropout_feature(hidden)
+                logits = self.output(hidden)
+                logits = logits.reshape(logits.shape[0], -1)
+
+                End_time = time.time()
+                Total_time = End_time - Start_time
+                # print(f"Time Records:\nTotal :{Total_time}, TFencoded:{TFencoded_time}, CnnRes:{CnnRes_time}, RnnRes:{RnnRes_time}, TFdecoded:{TFdecoded_time}, TFRes:{TFRes_time}")
+                pos = 0
+                for i in range(batch_size):
+                    if i in r:
+                        x_pred_logit[i] = logits[pos]
+                        pos += 1
+
+        return F.softmax(x_pred_logit, dim=1)
 
         # cosine output
         # logits = self.tau * cosine_classsifier(x=feature, w=self.base_weights)
